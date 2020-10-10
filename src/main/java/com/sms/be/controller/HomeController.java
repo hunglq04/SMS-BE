f790d8dbc787a842e2d3beef3051710885ee4e90
Hung <35440370+hunglq04@users.noreply.github.com>
2020-10-10 15:22:37 +0700

    Add request mapping

--- src/main/java/com/sms/be/controller/HomeController.java
+++ src/main/java/com/sms/be/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.sms.be.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @GetMapping("/")
     public ResponseEntity<String> home() {
         return ResponseEntity.ok("Herokuuuuuuuuuu");
     }
