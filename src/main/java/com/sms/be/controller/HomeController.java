4f69a042519b95f3dcc70762ad2a052bdbe5a50c
Hung <35440370+hunglq04@users.noreply.github.com>
2020-10-10 16:13:57 +0700

    Fix enviroment

--- src/main/java/com/sms/be/controller/HomeController.java
+++ src/main/java/com/sms/be/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.sms.be.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public ResponseEntity<String> home() {
-        return ResponseEntity.ok("Herokuuuuuuuuuu");
+    public String home() {
+        return "Herokuuuuuuuuuu";
     }
 }
