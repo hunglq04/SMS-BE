f472864741585af94e5a3aac3bccf763e1cfe2ab
Hung <35440370+hunglq04@users.noreply.github.com>
2020-10-11 21:53:28 +0700

    Allow cors from FE

--- src/main/java/com/sms/be/controller/HomeController.java
+++ src/main/java/com/sms/be/controller/HomeController.java
@@ -3,12 +3,15 @@ package com.sms.be.controller;
 import com.sms.be.model.Account;
 import com.sms.be.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+//TODO remove this test and
+@CrossOrigin(origins = {"http://localhost:4200", "https://sms-fe.web.app"})
 @RestController
 public class HomeController {
 
