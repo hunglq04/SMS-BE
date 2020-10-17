26badcafcabd695bcb5e10003026875827116bd3
Hung <35440370+hunglq04@users.noreply.github.com>
2020-10-17 12:06:11 +0700

    SMS-10: Config flyway

--- src/main/java/com/sms/be/BeApplication.java
+++ src/main/java/com/sms/be/BeApplication.java
@@ -1,13 +1,25 @@
 package com.sms.be;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class BeApplication {
+public class BeApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(BeApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
 }
