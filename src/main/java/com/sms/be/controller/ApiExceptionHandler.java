336028f039b0a4ec0815dab82b473537602c754e
Hung <35440370+hunglq04@users.noreply.github.com>
2020-10-24 10:18:37 +0700

    SMS-19: Add migration script to insert data of province, district, ward in Vietnam

--- src/main/java/com/sms/be/controller/ApiExceptionHandler.java
+++ src/main/java/com/sms/be/controller/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sms.be.controller;
 
 import com.sms.be.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +15,12 @@ public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse unexpectedException(Exception ex, WebRequest request) {
-        return new ErrorResponse(500, ex.getMessage(), request.getContextPath(), LocalDateTime.now());
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), request.getContextPath(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse badCredentialException(Exception ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getContextPath(), LocalDateTime.now());
     }
 }
