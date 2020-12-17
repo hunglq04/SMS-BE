e69f937643461a4b60398e0e5fc2082db37bdcde
Hung <35440370+hunglq04@users.noreply.github.com>
2020-12-17 20:56:59 +0700

    SMS-54; Fix base64 build erro

--- src/main/java/com/sms/be/config/MailConfig.java
+++ src/main/java/com/sms/be/config/MailConfig.java
@@ -1,12 +1,11 @@
 package com.sms.be.config;
 
 import com.sms.be.constant.CommonConstants;
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import java.util.Base64;
 
 import java.util.Properties;
 
@@ -14,14 +13,14 @@ import java.util.Properties;
 public class MailConfig {
 
     @Bean
-    public JavaMailSender getJavaMailSender() throws Base64DecodingException {
+    public JavaMailSender getJavaMailSender() {
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
         mailSender.setUsername(CommonConstants.ADDRESS);
-        mailSender.setPassword(new String(Base64.decode(CommonConstants.CODE)));
+        mailSender.setPassword(new String(Base64.getDecoder().decode(CommonConstants.CODE)));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
