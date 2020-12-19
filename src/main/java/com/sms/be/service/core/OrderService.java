817f2b881d57c86284d7bc4dd37c9e40a0ec40dc
hoangtram <hoangtramdt51@gmail.com>
2020-12-19 14:20:22 +0700

    SMS-56: Create Api get orderhistory

--- src/main/java/com/sms/be/service/core/OrderService.java
+++ src/main/java/com/sms/be/service/core/OrderService.java
@@ -2,6 +2,7 @@ package com.sms.be.service.core;
 
 import com.sms.be.dto.request.OrderRequest;
 import com.sms.be.dto.response.OrderResponse;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface OrderService {
     List<OrderResponse> getOrderHistorytByCustomer();
     void orderProducts(OrderRequest orderRequest);
     OrderResponse getOrderDetail(Long orderId);
+    Page<OrderResponse> getOrderPageByDate(int pageSize, int pageOffset, String fromDate);
 }
